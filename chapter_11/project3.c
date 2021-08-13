4b8bb3811ca851f14f19b34942d808977c1cd508
giftcup <salometambe@gmail.com>
2021-08-13 23:39:27 +0100

    More info in file.

---
+++ chapter_11/project3.c
@@ -0,0 +1,49 @@
+/* Description: Reduces a fraction to the lowest terms.
+** Source: C Programming A Modern Approach
+*/
+
+#include <stdio.h>
+
+void reduce(int numerator, int denominator, int* reduced_numerator, int* reduced_denominator);
+
+int main(void)
+{
+    int numerator, denominator;
+    int reduced_numerator, reduced_denominator;
+
+    printf("Enter a fraction: ");
+    scanf("%d/%d", &numerator, &denominator);
+    reduced_numerator = numerator;
+    reduced_denominator = denominator;
+    
+    reduce(numerator, denominator, &reduced_numerator, &reduced_denominator);
+
+    printf("In lowest terms: %d/%d\n", reduced_numerator, reduced_denominator);
+}
+
+void reduce(int numerator, int denominator, int* reduced_numerator, int* reduced_denominator)
+{
+    if (denominator > numerator)
+    {
+        int temp = denominator;
+        denominator = numerator;
+        numerator = temp;
+    }
+
+    do
+    {
+        if(denominator == 0)
+        {
+            *reduced_numerator /= numerator;
+            *reduced_denominator /= numerator;
+            printf("In lowest terms: %d/%d\n", *reduced_numerator, *reduced_denominator);
+            return;
+        }
+        else
+        {
+            int t = numerator;
+            numerator = denominator;
+            denominator = t % denominator;
+        }
+    } while (denominator >= 0);
+}
