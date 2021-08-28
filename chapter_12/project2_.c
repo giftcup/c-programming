a2bf60b8ee7d249a93edb7c8ae40fb3ff6d5e82b
giftcup <salometambe@gmail.com>
2021-08-28 13:06:54 +0100

    Used a single array to check if sentence is a palindrome.

---
+++ chapter_12/project2_.c
@@ -0,0 +1,45 @@
+/* Checks if a sentence is a palindrome */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define N 100
+
+bool palindrome(char* arr);
+
+int main(void)
+{
+    char arr[N];
+
+    printf("Enter a message: ");
+    bool pal = palindrome(arr);
+
+    if (pal)
+        printf("Palindrome\n");
+    else 
+        printf("Not a palindrome\n");
+
+    return 0;
+}
+
+bool palindrome(char* arr)
+{
+    int i = 0;
+    char ch;
+
+    while ((ch = getchar()) != '\n' && i <  N)
+    {
+        if(isalpha(ch))
+            arr[i++] = tolower(ch);
+    }
+    
+    int j = 0;
+    while (j < i)
+    {
+        if (arr[j] != arr[i-j-1])
+            return false;
+        j++;
+    }
+    return true;
+}
