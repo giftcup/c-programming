3ea75e4937cf97b389e7f33cf466aedb2df021ea
giftcup <salometambe@gmail.com>
2021-09-14 18:09:33 +0100

    Description in file

---
+++ chapter_14/exercises/exercise5.c
@@ -0,0 +1,28 @@
+/* Evidence of incorrect results when arguments with
+** side effects are passed to macros. The macro eva-
+** luates the argument more than once, hence, 
+** producing unexpected results
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define TOUPPER(c) ('a' <= (c) && (c) <= 'z'? (c) - 'a' + 'A': (c))
+
+int main(void)
+{
+    char s[5];
+    int i;
+
+    strcpy(s, "abcd");
+    i = 0;
+    putchar(TOUPPER(s[++i]));
+    putchar('\n');
+
+    strcpy(s, "0123");
+    i = 0;
+    putchar(TOUPPER(s[++i]));
+    putchar('\n');
+
+    return 0;
+}
