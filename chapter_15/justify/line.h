99f2e4d352568bb43bcefb17ed58544d7dc7f50d
giftcup <salometambe@gmail.com>
2021-09-24 22:08:58 +0100

    Formats a file of text.

---
+++ chapter_15/justify/line.h
@@ -0,0 +1,19 @@
+#ifndef LINE_H
+#define LINE_H
+
+/* clear_line: Clears the current line */
+void clear_line(void);
+
+/* add_word: Adds word to the end of current line */
+void add_word(const char *word);
+
+/* space_remaining: Returns the number of characters left in the current line */
+int space_remaining(void);
+
+/* write_line: Writes the current line with justification */
+void write_line(void);
+
+/* flush_line: Writes the current line without justification. IFf the line is empty does nothing. */
+void flush_line(void);
+
+#endif
