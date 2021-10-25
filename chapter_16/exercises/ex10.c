c875013e40c73ebb0c79b641387ac93fc3c87207
giftcup <salometambe@gmail.com>
2021-10-25 17:45:58 +0100

    Completed exercise 10

--- chapter_16/exercises/ex10.c
+++ chapter_16/exercises/ex10.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct point{
     int x;
@@ -13,6 +14,9 @@ struct rectangle {
 
 int area(struct rectangle r);
 struct point center(struct rectangle r);
+void scale(struct rectangle *r, int x, int y);
+bool in_rectangle(struct rectangle r, struct point p);
+
 
 int main(void)
 {
@@ -28,4 +32,31 @@ int area(struct rectangle r)
     width = abs(r.upper_left.y - r.lower_right.y);
 
     return (len * width);
+}
+
+struct point center(struct rectangle r)
+{
+    struct point center;
+
+    center.x = abs(r.upper_left.x + r.lower_right.x) / 2;
+    center.y = abs(r.upper_left.y + r.lower_right.y) / 2;
+
+    return center;
+}
+
+void scale(struct rectangle *r, int x, int y)
+{
+    (*r).upper_left.x += x;
+    (*r).lower_right.x += x;
+
+    (*r).upper_left.y += y;
+    (*r).lower_right.y += y;
+}
+
+bool in_rectangle(struct rectangle r, struct point p)
+{
+    if ((p.x > r.upper_left.x && p.x < r.lower_right.x) &&
+        (p.y > r.upper_left.y && p.y < r.lower_right.y))
+        return true;
+    return false;
 }
