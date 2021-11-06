216136f51e1a15ebd57a9916aede1e308e315942
giftcup <salometambe@gmail.com>
2021-11-06 14:33:13 +0100

    Modified code to add node to the end of the list without initializing the head in main

--- chapter_17/add_node.c
+++ chapter_17/add_node.c
@@ -6,7 +6,7 @@ typedef struct list {
     struct list *next_node;
 } List;
 
-void add_to_end(List* tracker, int number);
+List* add_to_end(List* tracker, int number);
 
 int main(void)
 {
@@ -14,18 +14,21 @@ int main(void)
     head = NULL;
     tracker = NULL;
 
-    head = (List*) malloc(sizeof(List));
-    head->number = 1;
-    head->next_node = NULL;
-    tracker = head;
+    for (int i = 1; i < 6; i++)
+    {
+        if (head == NULL)
+        {
+            head = add_to_end(head, i);
+            tracker = head;
+            continue;
+        }
+        tracker->next_node = add_to_end(tracker, i);
+        tracker = tracker->next_node;
+    }
 
-    for (int i = 2; i < 6; i++)
-         add_to_end(tracker, i);
-
-    for (int i = 1; i < 6  && head != NULL; i++)
+    for (; head != NULL; head = head->next_node)
     {
         printf("%d->", head->number);
-        head = head->next_node;
     }
     if (head == NULL)
         printf("NULL\n");
@@ -33,7 +36,7 @@ int main(void)
     return 0;
 }
 
-void add_to_end(List* tracker, int number)
+List* add_to_end(List* tracker, int number)
 {
     List *new_node;
     new_node = (List*) malloc(sizeof(List));
@@ -41,11 +44,21 @@ void add_to_end(List* tracker, int number)
     if (new_node == NULL)
     {
         printf("memory allocation failed in add_to_end\n");
-        return;
+        return NULL;
+    }
+    new_node->number = number;
+
+    if (tracker == NULL)
+    {
+        tracker = new_node;
     }
 
-    new_node->number = number;
-    tracker->next_node = new_node;
-    tracker = new_node;
+    else
+    {
+        tracker->next_node = new_node;
+        tracker = new_node;
+    }
     tracker->next_node = NULL;
+
+    return tracker;
 }
