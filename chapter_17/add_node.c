23b6e41f16c70861c4f885dad254b61d47924670
giftcup <salometambe@gmail.com>
2021-11-07 22:31:43 +0100

    Swapped first two elements

--- chapter_17/add_node.c
+++ chapter_17/add_node.c
@@ -7,30 +7,35 @@ typedef struct list {
 } List;
 
 void add_to_end(List** tracker, int number);
+void delete_node(List** tracker, int number);
+void print_node(List* head);
+// void bubble_sort(List** tracker);
+void swap(List** head);
 
 int main(void)
 {
     List *head, *tracker;
+    int number;
     head = NULL;
     tracker = NULL;
 
-    for (int i = 1; i < 6; i++)
+    printf("Enter 0 to quit: ");
+    for (;;)
     {
+        scanf("%d", &number);
+        if (number == 0)
+            break;
         if (head == NULL)
         {
-            add_to_end(&head, i);
+            add_to_end(&head, number);
             tracker = head;
             continue;
         }
-        add_to_end(&tracker, i);
+        add_to_end(&tracker, number);
     }
-
-    for (; head != NULL; head = head->next_node)
-    {
-        printf("%d->", head->number);
-    }
-    if (head == NULL)
-        printf("NULL\n");
+    print_node(head);
+    swap(&head);
+    print_node(head);
 
     return 0;
 }
@@ -48,14 +53,47 @@ void add_to_end(List** tracker, int number)
     new_node->number = number;
 
     if (*tracker == NULL)
-    {
         *tracker = new_node;
-    }
-
     else
     {
         (*tracker)->next_node = new_node;
         *tracker = new_node;
     }
     (*tracker)->next_node = NULL;
+}
+
+void delete_node(List** tracker, int number)
+{
+    List *cur, *prev;
+    cur = prev = (List*)malloc(sizeof(List));
+
+    for (cur = *tracker, prev = NULL; 
+         cur != NULL && cur->number != number;
+         prev = cur, cur = cur->next_node)
+    ;
+    if (cur == NULL) return;
+    if (prev == NULL)
+        *tracker = cur->next_node;
+    else
+        prev->next_node = cur->next_node;
+
+    free(cur);
+}
+
+void print_node(List* head)
+{
+    for (; head != NULL; head = head->next_node)
+        printf("%d->", head->number);
+    if (head == NULL)
+        printf("NULL\n");
+}
+
+void swap(List** head)
+{
+    List* next;
+
+    next = (*head)->next_node;
+    (*head)->next_node = next->next_node;
+    next->next_node = (*head);
+    (*head) = next;
 }
