b991eb0f0e5008d9a0284319f16f9a4dd717bc01
giftcup <salometambe@gmail.com>
2021-11-30 23:20:43 +0100

    An inventory

--- chapter_16/inventory/inventory.c
+++ chapter_16/inventory/inventory.c
@@ -19,3 +19,50 @@ void insert(void);
 void search(void);
 void update(void);
 void print(void);
+
+int main(void) {
+    char code;
+
+    for (;;) {
+        printf("Enter operation code: ");
+        scanf(" %c", &code);
+        while (getchar() != '\n')       /* skips to end of line */
+            ;
+        switch (code) {
+            case 'i': 
+                insert();
+                break;
+            case 's': 
+                search();
+                break;
+            case 'u':
+                update();
+                break;
+            case 'p':
+                print();
+                break;
+            case 'q':
+                return 0;
+            default:
+                printf("Illegal code\n"); 
+        }
+        printf("\n");
+    }
+}
+
+int find_part(int number) {
+    int i;
+
+    for (i = 0; i < num_parts; i++)
+        if (inventory[i].number == number)
+            return i;
+        return -1;
+}
+
+void insert(void) {
+    int part_number;
+
+    if (num_parts == MAX_PARTS) {
+        printf("Datanase is full: can't add more parts.\n");
+    }
+}
