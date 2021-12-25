c004185917f90929e5e3e2a0b972f9bfa30be6ad
giftcup <salometambe@gmail.com>
2021-12-25 21:49:58 +0100

    reading a string without knowing the length and storing in a flexible array member

--- chapter_17/exercises/ex8.c
+++ chapter_17/exercises/ex8.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 typedef struct stack {
     int token;
     struct stack *next;
+    char string[];
 } Stack;
 
 void make_empty(void);
@@ -14,13 +16,26 @@ void push(int i);
 int pop(void);
 
 int main (void) {
-    char token;
+    char *token, ch;
+    int size_of_token;
     Stack *head;
 
-    while ((token = getchar()) != '\n') {
-        if (isalnum(token)) {
-            printf("yay!\n");
-            // push(head);
+    token = malloc(sizeof(char) * 1);
+    size_of_token = 1;
+    token[0] = '\0';
+
+    while ((ch = getchar()) != '\n') {
+        if (ch == ' ') {
+            head = malloc(sizeof(Stack) + sizeof(char) * strlen(token));
+            strcpy(head->string, token);
+            memset(token, 0, strlen(token));
+            printf("%s\n", head->string);
+            continue;
+        } else {
+            size_of_token++;
+            token = realloc(token, sizeof(char) * size_of_token);
+            strncat(token, &ch, 1);
+            printf("%s\n", token);
         }
     }
 }
