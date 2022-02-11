66d0bcc034053f60a234c94f14a6a0558eafb553
giftcup <salometambe@gmail.com>
2022-02-11 15:36:35 +0100

    change the size of all the words to the max size to each sorting

--- chapter_17/projects/word_sort/word_sort.c
+++ chapter_17/projects/word_sort/word_sort.c
@@ -3,9 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include "read_word.h"
+#include "sort.h"
 
 #define MAX_WORD_LEN 20
-#define MAX_NUM_WORDS 20
+#define MAX_NUM_WORDS 3
 
 int main(void) {
     char *words[MAX_NUM_WORDS];
@@ -19,7 +20,7 @@ int main(void) {
         if (!word_len)
             break;
         
-        words[num_words] = malloc(sizeof(char) * strlen(word) + 1);
+        words[num_words] = malloc(sizeof(char) * MAX_WORD_LEN + 1);
         strcpy(words[num_words], word);
     }
     if (num_words == MAX_NUM_WORDS)
@@ -27,6 +28,8 @@ int main(void) {
                "+++++++++++ Sorry, you can't put in more words :( ++++++++++++++\n"
                "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n");
 
+    sort(words, num_words);
+    printf("In sorted order: ");
     for (j = 0; j < num_words; j++) {
         printf("%s ", words[j]);
     } putchar('\n');
