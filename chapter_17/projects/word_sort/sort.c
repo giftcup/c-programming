861418bdd9c6aa1ac9fe4e5a17150055af670760
giftcup <salometambe@gmail.com>
2022-02-14 22:42:33 +0100

    include the max lengthas a parameter to sort.c

--- chapter_17/projects/word_sort/sort.c
+++ chapter_17/projects/word_sort/sort.c
@@ -3,24 +3,23 @@
 #include <stdio.h>
 #include "sort.h"
 
-void sort(char **words, int num_words) {
+void sort(char **words, int num_words, int word_length) {
     int i, j;
 
     for (i = 0; i < num_words; i++) {
-        for (j = 0; j < num_words-i; j++)
+        for (j = 0; j < num_words-i-1; j++)
             if (strcmp(words[j], words[j+1]) > 0)
-                swap(words[j], words[j+1]);
+                swap(words[j], words[j+1], word_length);
     }
     
     return;
 }
 
-void swap(char *word1, char *word2) {
+void swap(char *word1, char *word2, int word_length) {
     char *tmp_word;
-
-    tmp_word = malloc(sizeof(char) * strlen(word1) + 1);
+    tmp_word = malloc(sizeof(char) * word_length + 1);
+    
     strcpy(tmp_word, word1);
     strcpy(word1, word2);
     strcpy(word2, tmp_word);
-    
 }
--- chapter_17/projects/word_sort/sort.h
+++ chapter_17/projects/word_sort/sort.h
@@ -1,2 +1,2 @@
-void sort(char **words, int num_words);
-void swap(char *word1, char *word2);
+void sort(char **words, int num_words, int word_length);
+void swap(char *word1, char *word2, int word_length);
